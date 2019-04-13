278315a99ba4664926e7657bf792d57e752a86b2
lvao <1137804712@qq.com>
2019-04-13 13:56:36 +0800

    采用tabsmenu插件重组横向导航栏

--- config/initial.js
+++ config/initial.js
@@ -7,6 +7,18 @@ $(".sidebar-collapse").slimScroll({
 	alwaysVisible: !1
 });
 
+// tabs 加载
+$("#nav-tabs").tabsMenu({
+	kind:".J_menuItem",
+	activeKind:"metismenu-active",
+	fixed_tab:{
+		icon:"fa fa-home",
+		src:"pages/dashboard/dashboard.html"
+	},
+	close_btns:true,
+	tabsCallback: pageLoad
+});
+
 // 菜单按钮控制切换
 $(".navbar-minimalize").click(function() {
 	$("body").toggleClass("mini-sidebar");
@@ -14,16 +26,16 @@ $(".navbar-minimalize").click(function() {
 	SmoothlyMenu();
 });
 
-function NavToggle() {
-	$(".navbar-minimalize").trigger("click");
-}
-
 function SmoothlyMenu() {
 	$("body").hasClass("mini-sidebar") ? ($("#side-menu").hide(), setTimeout(function() {
 		$("#side-menu").fadeIn(500);
 	}, 300)) : ($("#side-menu").hide(), setTimeout(function() {
 		$("#side-menu").fadeIn(500);
-	}, 100));
+	}, 300));
+}
+
+function NavToggle() {
+	$(".navbar-minimalize").trigger("click");
 }
 
 $("#side-menu>li").click(function(e) {
@@ -36,44 +48,6 @@ $("#side-menu>li li a").click(function() {
 
 /(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) && $("#content-main").css("overflow-y", "auto");
 
-// 关闭选项卡按钮
-$("a.cartbtn-switcher").click(function() {
-	$(this).find('.fa').toggleClass("fa-caret-down").toggleClass("fa-caret-up");
-	$(this).next().toggle();
-});
-
-// 导航菜单链接及选项卡和初始化
-var homeSrc = $('div[name="iframe0"]').attr('src');
-pageLoad(homeSrc, 'init');
-selectTab(homeSrc);
-
-// 为menuitem 添加data-index 属性
-$(".J_menuItem").each(function(k) {
-	if(!$(this).attr("data-index")) {
-		$(this).attr("data-index", k);
-	}
-});
-
-// 点击 menuitem 加载对应页面
-$(".J_menuItem").on("click", triggerMIeffects);
-
-// 点击menuTabs 下 a 标签
-$(".J_menuTabs").on("click", ".J_menuTab", triggerMTeffects);
-
-// 点击menuTabs 下关闭按钮 
-$(".J_menuTabs").on("click", ".J_menuTab i", triggerMTclose);
-
-// 前一个按钮选项卡
-$(".J_tabLeft").on("click", triggerPrevBtn);
-
-// 后一个按钮选项卡
-$(".J_tabRight").on("click", triggerNextBtn);
-
-// 关闭操作选项卡 --关闭所有选项卡 -- 事件
-$(".J_tabCloseAll").on("click", triggerCloseAll);
-
-// 关闭操作选项卡 --关闭其他选项卡-- 事件
-$(".J_tabCloseOther").on("click", triggerCloseOther);
 
 // 
 $('#content-main').on('click', '.collapse-link', function() {
@@ -94,266 +68,32 @@ $('#content-main').on('click', '.close-link', function() {
 	o.remove();
 });
 
+// 主题页面初始化
+var home_src = $('.J_iframe').data("src");
+var options = {
+	current: home_src
+}
+pageLoad(options);
+
+
 // 页面加载函数
-function pageLoad(src, status) {
-	var pageName = src.split('/').pop().split('.')[0];
+function pageLoad(options) {
+	var current = options.current;
+	var pageName = current.split('/').pop().split('.')[0];
 	router({
 		pageName: pageName,
-		status: status,
-		el: $('.J_iframe[src="' + src + '"]')
+		el: $('.J_iframe')
 	});
 }
 
-function selectTab(src) {
-	var current = $('a.J_menuItem[href="' + src + '"]');
-	if(src){
-		$("a.J_menuItem").removeClass("metismenu-active");
-		current.addClass("metismenu-active");
-	}
-}
 
-// menuitem点击事件 响应函数
-function triggerMIeffects() {
-	var src = $(this).attr("href"),
-		index = $(this).data("index"),
-		txt = $.trim($(this).text()),
-		exsit_opt = true;
-	if(src == undefined || $.trim(src).length == 0) {
-		return false;
-	}
-	$(".J_menuTab").each(function() {
-		if($(this).data("id") == src && !$(this).hasClass("active")) {
-			var prev_src = $(this).parent().find(".J_menuTab.active").data("id");
-			pageLoad(prev_src, "out");
-			$(this).addClass("active").siblings(".J_menuTab").removeClass("active");
-			dealWidth($(this));
-			$(".J_mainContent .J_iframe").each(function() {
-				if($(this).data("id") == src) {
-					$(this).show().siblings(".J_iframe").hide();
-					pageLoad(src, "in");
-					return false;
-				}
-			})
-			exsit_opt = false;
-			return false;
-		}
-		if($(this).data("id") == src && $(this).hasClass("active")){
-			exsit_opt = false;
-			return false;
-		}
-	});
-	if(exsit_opt) {
-		pageLoad($(".J_menuTab.active").data("id"), "out");
-		var tab_a = '<a href="javascript:;" class="active J_menuTab" data-id="' + src + '">' + txt + ' <i class="fa fa-close"></i></a>';
-		$(".J_menuTab").removeClass("active");
-		var frame_div = $('<div class="J_iframe" name="iframe' + index + '" width="100%" height="100%" src="' + src + '" frameborder="0" data-id="' + src + '" seamless></div>');
-		$(".J_mainContent").find("div.J_iframe").hide().parents(".J_mainContent").append(frame_div);
-		$(".J_menuTabs .page-tabs-content").append(tab_a);
-		dealWidth($(".J_menuTab.active"));
-		pageLoad(src, "init");
-	}
-	selectTab(src);
-	return false;
-}
 
-// menutab 点击事件 响应函数
-function triggerMTeffects() {
-	var self = $(this);
-	var src = self.data("id");
-	if(!self.hasClass("active")) {
-		var prev_src = self.siblings(".J_menuTab.active").data("id");
-		pageLoad(prev_src, "out");
-		$(".J_mainContent .J_iframe").each(function() {
-			if($(this).data("id") == src) {
-				$(this).show().siblings(".J_iframe").hide();
-				pageLoad(src, "in");
-				return false;
-			}
-		});
-		self.addClass("active").siblings(".J_menuTab").removeClass("active");
-		dealWidth(this);
-	}
-	selectTab(src);
-}
 
-// 前进定位选项卡
-function triggerPrevBtn() {
-	var prev = $(".J_menuTab.active").prev();
-	var src;
 
-	if(prev.length) {
-		prev.addClass("active").siblings().removeClass("active");
-		src = prev.data("id");
-	} else {
-		console.log("1111");
-		$(".J_menuTab:last").addClass("active").siblings().removeClass("active");
-		src = $(".J_menuTab:last").data("id");
-	}
-	
-	dealWidth($(".J_menuTab.active"));
-	
-	$(".J_mainContent .J_iframe").each(function() {
-		if($(this).data("id") == src) {
-			$(this).show().siblings(".J_iframe").hide();
-			pageLoad(src, 'in');
-			return false;
-		}
-	});
-}
 
-// 下一个定位选项卡
-function triggerNextBtn() {
-	var next = $(".J_menuTab.active").next();
-	var src;
 
-	if(next.length) {
-		next.addClass("active").siblings().removeClass("active");
-		src = next.data("id");
-	} else {
-		$(".J_menuTab:first").addClass("active").siblings().removeClass("active");
-		src = $(".J_menuTab:first").data("id");
-	}
-	
-	dealWidth($(".J_menuTab.active"));
-	
-	$(".J_mainContent .J_iframe").each(function() {
-		if($(this).data("id") == src) {
-			$(this).show().siblings(".J_iframe").hide();
-			pageLoad(src, 'in');
-			return false;
-		}
-	});
-}
 
-// 横向选项卡超长处理
-function dealWidth(that) {
-	var otherWh = getWidth($(".content-tabs").children().not(".J_menuTabs"));
-	var firstWh = $(".J_menuTab:first").outerWidth();
-	var tabsWh = $(".content-tabs").outerWidth(true) - otherWh - firstWh;
-	var prevWh = getWidth($(that).prevAll()) - firstWh;
-	var mlWh = parseInt($(".page-tabs-content").css("margin-left"));
-	var actWh = $(that).outerWidth(true);
-	var overWh = mlWh;
-	var isOverMlFlag = (prevWh + actWh + mlWh - firstWh - tabsWh) > 0; 
-	var isOverMrFlag = (prevWh + mlWh - firstWh) < 0 ;
-	if($(that).index() == $(".content-tabs:first").index()){
-		overWh = firstWh;
-	}else{
-		if(isOverMlFlag) {
-			overWh = firstWh - (prevWh + actWh - tabsWh);
-			var widths = 0;
-			$(that).prevAll().each(function() {
-				widths += $(this).outerWidth(true);
-				if(widths > (firstWh-overWh)) {
-					overWh = firstWh - widths;
-					return false;
-				}
-			});
-		}else if(isOverMrFlag){
-			overWh = firstWh - prevWh;
-			console.log('111');
-		} else {
-			overWh = mlWh;
-		}
-	}
-	$(".page-tabs-content").animate({
-		marginLeft: overWh + "px"
-	}, "fast");
-}
 
-// 获取元素宽度
-function getWidth(thats) {
-	var widths = 0;
-	$(thats).each(function() {
-		widths += $(this).outerWidth(true); // outerWidth(true) 包含margin的width
-	});
-	return widths;
-}
 
-// menutab 关闭事件 响应函数
-function triggerMTclose() {
-	var selfa = $(this).parents(".J_menuTab");
-	var src = selfa.data("id");
-	var wh = selfa.width();
-	if(selfa.hasClass("active")) {
-		if(selfa.next(".J_menuTab").length) {
-			var next_src = selfa.next().data("id");
-			selfa.next().addClass("active");
-			$(".J_mainContent .J_iframe").each(function() {
-				if($(this).data("id") == next_src) {
-					$(this).show().siblings(".J_iframe").hide();
-					return false;
-				}
-			});
-			selfa.remove();
-			$(".J_mainContent .J_iframe").each(function() {
-				if($(this).data("id") == src) {
-					$(this).remove();
-					pageLoad(src, 'destroy');
-					return false;
-				}
-			});
-			selectTab(next_src);
-		}
-		if(selfa.prev().length) {
-			var prev_src = selfa.prev().data("id");
-			selfa.prev().addClass("active");
-			selfa.remove();
-			$(".J_mainContent .J_iframe").each(function() {
-				if($(this).data("id") == src) {
-					$(this).remove();
-					pageLoad(src, 'destroy');
-					return false;
-				}
-			});
-			$(".J_mainContent .J_iframe").each(function() {
-				if($(this).data("id") == prev_src) {
-					$(this).show().siblings(".J_iframe").hide();
-					pageLoad(prev_src, "in");
-					return false;
-				}
-			});
-			selectTab(prev_src);
-		}
-	} else {
-		selfa.remove();
-		$(".J_mainContent .J_iframe").each(function() {
-			if($(this).data("id") == src) {
-				$(this).remove();
-				pageLoad(src, 'destroy');
-				return false;
-			}
-		});
-		dealWidth($(".J_menuTab.active"));
-	}
-	return false;
-}
 
-// 关闭所有选项卡
-function triggerCloseAll() {
-	$(".page-tabs-content").children("[data-id]").not(":first").each(function() {
-		var src = $(this).data("id");
-		var iframe = $('.J_iframe[data-id="' + src + '"]');
-		iframe.remove();
-		$(this).remove();
-		pageLoad(src, 'destroy');
-	});
-	$(".page-tabs-content").children("[data-id]:first").each(function() {
-		var src = $(this).data("id");
-		$('.J_iframe[data-id="' + src + '"]').show();
-		$(this).addClass("active");
-		pageLoad(src, 'in');
-	});
-}
-
-// 关闭除选中的其他选项卡
-function triggerCloseOther() {
-	$(".page-tabs-content").children("[data-id]").not(":first").not(".active").each(function() {
-		var dataId = $(this).data("id");
-		var iframe = $('.J_iframe[data-id="' + dataId + '"]');
-		iframe.remove();
-		$(this).remove();
-		pageLoad(dataId, 'destroy');
-	});
-}
 
--- config/main.js
+++ config/main.js
@@ -13,6 +13,7 @@ require.config({
 		layui: 'layui-src/dist/layui',
 		metisMenu: 'metismenu/dist/metisMenu',
 		slimscroll: 'jquery-slimscroll/jquery.slimscroll',
+		tabsMenu: 'tabsmenu/dist/tabsMenu.min',
 		echarts: 'echarts/dist/echarts',
 		echartsMapChina: 'echarts/map/js/china',
 		highcharts: 'highcharts/highcharts',
@@ -39,6 +40,9 @@ require.config({
 		},
 		slimscroll: {
 			deps: ['jquery']
+		},
+		tabsMenu:{
+			deps:['css!tabsmenu/dist/tabsmenu.min.css']
 		},
 		fancybox: {
 			deps: ['css!../node_modules/fancybox/dist/css/jquery.fancybox.css']
@@ -75,7 +79,7 @@ require.config({
 });
 
 // 框架加载
-require(['jquery', 'layui', 'metisMenu', 'slimscroll'], function() {
+require(['jquery', 'layui', 'metisMenu', 'slimscroll', 'tabsMenu'], function() {
 	layui.use(['element', 'layer'], function() {
 		var element = layui.element;
 		var layer = layui.layer;
@@ -86,56 +90,34 @@ require(['jquery', 'layui', 'metisMenu', 'slimscroll'], function() {
 // 记录打开页面的数组对象
 window['pages'] = [];
 
-// 页面作用域
-window.scope = {};
-
 // 模拟路由
-function router(param) { // pageName为moduleName，src完整路径，state页面执行状态
-	var reqMoudelName = '../pages/' + param.pageName + '/' + param.pageName; // 匿名模块的模块名称，用于移除
-	if(param.status !== 'init') {
-		var cur = currentPage(reqMoudelName);
-		if(cur && typeof cur.page[param.status] === 'function') {
-			cur.page[param.status]();
-		}
-		if(param.status === 'in') { // 再次切进页面
-			setTitle(param.pageName);
-			setScope(cur.page);
-		}
-		if(param.status === 'destroy') { // 关闭页面
-			destroyPage(reqMoudelName);
-			cur && window.pages.splice(cur.index, 1);
-		}
-	} else { // 第一次进来
-		initMainContent(reqMoudelName, param);
-	}
+function router(param) {
+	var reqMoudelName = '../pages/' + param.pageName + '/' + param.pageName;
+	destroyPage();
+	initPage(reqMoudelName, param);
 }
 
 // 初始化页面html
-function initMainContent(reqMoudelName, param) {
+function initPage(reqMoudelName, param) {
 	window.pageloadCode = layer.load(2, {
 		shade: [0.5, '#fff']
 	});
 	require(['text!' + reqMoudelName + '.html'], function(text) {
 		param.el.html(text);
-		setTitle(param.pageName);
 		loadPage(reqMoudelName, param.pageName);
 	});
 }
 
-// 初始化页面js
+// 加载页面
 function loadPage(reqMoudelName, pageName) {
-	require([
-		reqMoudelName, // 动态参数
-		'css!' + reqMoudelName
-	], function(page) {
+	require([reqMoudelName,'css!' + reqMoudelName], function(page) {
 		layer.close(window.pageloadCode);
 		page = typeof page === 'object' ? page : {};
 		window.pages.push({
 			pageName: pageName,
-			reqMoudelName: reqMoudelName,
-			page: page
+			reqMoudelName: reqMoudelName
 		});
-		setScope(page);
+		// 由于echarts，highcharts 自定义书写返回的是一个init函数，并没有直接调用，所有需要在此调用
 		if(typeof page.init === "function") {
 			page.init();
 		}
@@ -143,42 +125,11 @@ function loadPage(reqMoudelName, pageName) {
 }
 
 // 销毁页面
-function destroyPage(reqMoudelName) {
-	requirejs.undef(reqMoudelName); // 销毁模块 // 依赖的模块不销毁
-	requirejs.undef('require-css/css!' + reqMoudelName); // 销毁css
-	$('link[href*="' + reqMoudelName + '"]').remove();
-}
-
-// 获得当前页
-function currentPage(name) {
-	var index_;
-	var page = window.pages.filter(function(item, index) {
-		if(item.reqMoudelName === name || item.pageName === name) {
-			index_ = index;
-			return true;
-		}
+function destroyPage() {
+	window.pages.forEach(function(item){
+		requirejs.undef(item.reqMoudelName); // 销毁模块 // 依赖的模块不销毁
+		requirejs.undef('require-css/css!' + item.reqMoudelName); // 销毁css
+		$('link[href*="' + item.reqMoudelName + '"]').remove();
 	});
-	if(index_ === undefined || page.length !== 1 || !page[0] || !page[0].page) return;
-	return {
-		index: index_,
-		page: page[0].page
-	}
-}
-
-// 设置作用域
-function setScope(exports) {
-	window.scope = {};
-	$.extend(window.scope, exports);
-	delete window.scope.init;
-	delete window.scope.in;
-	delete window.scope.out;
-	delete window.scope.destroy;
-}
-
-// 设置title
-function setTitle(name) {
-	var pageTitle = $('title').text().split('-')[0];
-	if(pageTitle.indexOf('#') < 0) window.pageTitle = pageTitle;
-	var title = $('.J_iframe #' + name).attr('name') || $('.J_iframe:visible .wrapper-content').attr('name');
-	$('title').text(window.pageTitle + (title ? '-' + title : ''));
+	window.pages = [];
 }
